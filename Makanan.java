5ba96d4a349ecc2266bde71345275fd458851db9
luthfiyaindah11 <luthfiyafithriani@gmail.com>
2023-03-10 07:55:48 +0700

    023_Luthfiya Indah F.

---
+++ Makanan.java
@@ -0,0 +1,9 @@
+
+//Luthfiya Indah Fithriani (202110370311023)
+
+public class Makanan extends Hidangan {
+    public String disantap(){
+    return this.getNamaHidangan() + " dimakan";
+    }
+    
+}
---
+++ Minuman.java
@@ -0,0 +1,9 @@
+
+//Luthfiya Indah Fithriani (202110370311023)
+
+public class Minuman extends Hidangan{
+    public String disantap(){
+    return this.getNamaHidangan()+ " diminum";
+    }
+    
+}
